acc9e9b8d14ad37a40369a450980c4259357dd0d
suriya <suriya.e.aaron@gmail.com>
2019-05-01 22:53:18 +0530

    with database integration

--- mqtt_sub.py
+++ mqtt_sub.py
@@ -13,14 +13,18 @@ ip = "35.244.17.132"
 sub_topic = "fleet/#"
 q = queue.Queue(maxsize=0)
 
-'''myclient = MongoClient("mongodb://localhost:27017/")
-mydb = myclient["fleet"]
-mycol = mydb["mqtt"]'''
 
 def mqtt_thread():
+    myclient = MongoClient("mongodb://localhost:27017/")
     while True:
-        data_1=q.get()
-        logging.debug(data_1)
+        data_1 = q.get()
+        try:
+            mydb = myclient[data_1[0]]
+            mydb.mqtt.insert_one(data_1[1])
+            logging.debug("Inserted successfully:"+str(data_1))
+        except Exception as e:
+            logging.debug(str(e))
+
 
 def on_connect(client, userdata, flags, rc):
     client.subscribe(sub_topic)
@@ -31,7 +35,7 @@ def on_message(client, userdata, msg):
     logging.debug("start time:"+str(datetime.now()))
     try:
         topic = (msg.topic).split("/")
-        data = [topic[1],(msg.payload).decode("utf-8")]
+        data = [topic[1], json.loads((msg.payload).decode("utf-8"))]
         q.put(data)
     except Exception as e:
         logging.debug("Error: "+str(e))
